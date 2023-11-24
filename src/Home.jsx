9e39c2b3a093a125b3f848371866d04bc35c3693
mariafer057 <131805494+mariafer057@users.noreply.github.com>
2023-11-24 13:46:52 -0400

    iniciando

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,26 @@
 export default function Home(){
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        <div>
+     <h1>PTAC 2 - PROJETO FINAL</h1>
+     <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+        </div>
+   
      );
 }
+//peguei o código anterior do meu projeto PTAC-2-AULA5
+//pegando o navbar do bootstrap
